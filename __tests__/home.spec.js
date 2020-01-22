f144f724ffede7b7cd6d3e879c948ecfe1efc7fc
Girma Nigusse <xgirma@gmail.com>
2020-01-21 21:19:34 -0500

    set CI

---
+++ __tests__/home.spec.js
@@ -0,0 +1,34 @@
+import puppeteer from "puppeteer";
+
+let page;
+let browser;
+
+describe("Sandbox", () => {
+  beforeAll(async () => {
+    browser = process.env.GITHUB_ACTIONS
+      ? await puppeteer.launch()
+      : await puppeteer.launch({ headless: false });
+
+    page = await browser.newPage();
+
+    await page
+      .goto("https://xgirma.github.io/sandbox/", {
+        waitUntil: "networkidle0"
+      })
+      .catch(() => {});
+  });
+
+  afterAll(() => {
+    if (!page.isClosed()) {
+      browser.close();
+    }
+  });
+
+  test("should be on the sandbox", async () => {
+    await page.waitFor("h1");
+    const title = await page.$eval("h1", el => el.textContent);
+
+    expect(await page.title()).toEqual("Sandbox");
+    expect(title).toEqual("Sandbox");
+  });
+});
